c892d8ba9c34b654640729dca56e606a2a5f7852
Habib Atrisan <46671942+habibartisan@users.noreply.github.com>
2019-04-12 15:53:57 +0600

    Create PHP lcfirst() Function.php

---
+++ PHP lcfirst() Function.php
@@ -0,0 +1,4 @@
+ucfirst() - converts the first character of a string to uppercase
+ucwords() - converts the first character of each word in a string to uppercase
+strtoupper() - converts a string to uppercase
+strtolower() - converts a string to lowercase
